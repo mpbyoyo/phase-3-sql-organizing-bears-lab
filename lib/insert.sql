310cf9c71e7da7a1bc61dfc5b3610fcc88bb6b9d
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-06-08 17:16:06 -0500

    commit tree f3e80eaf49367e6ecd449e5ecbb4b5e493c32f1e

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+  (1, "Mr. Chocolate", 12, "M", "brown", "angry", true),
+  (2, "Rowdy", 11, "M", "brown", "angry", true),
+  (3, "Tabitha", 13, "F", "red", "angry", true),
+  (4, "Sergeant Brown", 12, "M", "brown", "angry", true),
+  (5, "Melissa", 15, "F", "red", "angry", true),
+  (6, "Grinch", 16, "M", "red", "angry", true),
+  (7, "Wendy", 17, "F", "brown", "angry", true),
+  (8, NULL, 18, "M", "brown", "angry", true)
